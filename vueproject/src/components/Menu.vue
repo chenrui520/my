91443a8919fdf66058113bf2963e0670ca3b1778
chenrui <q2629558255@sina.com>
2020-02-17 20:10:18 +0800

    first

---
+++ vueproject/src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1 class="title">{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Menu extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+    width: 100%;
+    height: 100px;
+    .title {
+        color: #FF101010;
+        font-size: 24px;
+    }
+}
+</style>
---
+++ vueproject/src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Menu msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue'
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  props: {
+    propMessage: String
+  },
+  components: {
+    Menu
+  }
+})
+export default class App extends Vue {
+  // initial data
+  msg = 123
+
+  // use prop values for initial data
+  helloMsg = 'Hello, ' + this.propMessage
+
+  // lifecycle hook
+  mounted () {
+    this.greet()
+  }
+
+  // computed
+  get computedMsg () {
+    return 'computed ' + this.msg
+  }
+
+  // method
+  greet () {
+    alert('greeting: ' + this.msg)
+  }
+}
+
+</script>
